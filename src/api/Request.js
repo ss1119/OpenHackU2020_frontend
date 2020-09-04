9a19a85150256582b76cccbcac7f3aade531f7e0
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-09-04 23:19:00 +0900

    finish

--- src/api/Request.js
+++ src/api/Request.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
-// axios.defaults.baseURL = "https://open-hack-u-2020-backend.herokuapp.com/";
+axios.defaults.baseURL = "https://open-hack-u-2020-backend.herokuapp.com/";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 export function get(path) {
